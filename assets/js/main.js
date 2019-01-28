8fcad13fc48e263564970f81f50e6087603fb416
Jesus Estevez <32360649+jEstevezRod@users.noreply.github.com>
2019-01-28 21:16:33 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
         
         $("#flip").click(() => {
-            let array1 = ['Jesus 1', 'Kamal', 'Jesus 2','Aurelien','Miriam','Sabina'];
+//             let array1 = ['Jesus 1', 'Kamal', 'Jesus 2','Aurelien','Miriam','Sabina'];
+                let array1 = ["Jesus", "Marina la cateadora"];
             let ran = Math.floor(Math.random() * array1.length);
             alert("The winner is " + array1[ran])
         });
